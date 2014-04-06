cfeaceeaab57cdfb2df9978ad8ba5d64a7807283
metagoto <runpac314@gmail.com>
2014-04-06 21:18:57 +0200

    reorganize tests stuff

--- test/map_val.cpp
+++ test/map_val.cpp
@@ -1,40 +1,44 @@
-#pragma once
 
+#include <iostream>
 #include <jsxx/core/basic_val.hpp>
 #include <jsxx/core/functions.hpp>
 #include <jsxx/core/operators.hpp>
 #include <jsxx/core/iterators.hpp>
 #include <jsxx/core/handlers.hpp>
+#include <jsxx/reader.hpp>
+//#include <jsxx/writer.hpp>
+#include <cstring>
 #include <string>
 #include <vector>
+#include <map>
+#include "lest.hpp"
 
 
-namespace jsxx
+using namespace jsxx;
+
+template<typename Val>
+struct trait<Val, struct map_val_tag> {
+  using bool_t = bool;
+  using int_t = int;
+  using real_t = double;
+  using string_t = std::string;
+  using pair_t = std::pair<string_t, Val>;
+  using array_t = std::vector<Val>;
+  using object_t = std::map<string_t, Val>;
+};
+
+using val = basic_val<map_val_tag>;
+
+namespace lit
 {
-
-  template<typename Val>
-  struct trait<Val, struct simple_val_tag> {
-    using bool_t = bool;
-    using int_t = int;
-    using real_t = double;
-    using string_t = std::string;
-    using pair_t = std::pair<string_t, Val>;
-    using array_t = std::vector<Val>;
-    using object_t = std::vector<pair_t>;
-  };
-
-  using val = basic_val<simple_val_tag>;
-
-  namespace val_literals
+  inline constexpr val::k operator"" _k(val::char_t const* s, std::size_t) noexcept
   {
-    inline constexpr val::k operator"" _k(val::char_t const* s, std::size_t) noexcept
-    {
-      return val::k(s);
-    }
+    return val::k(s);
   }
+}
 
-  template<>
-  struct decode_handler<val>
+namespace jsxx {
+  template<> struct decode_handler<val>
   {
     template<typename It, typename T>
     static int integer(It beg, It end, T& result) noexcept {
@@ -56,5 +60,16 @@ namespace jsxx
       return decode_string(beg, end, result);
     }
   };
-
 }
+
+using namespace lit;
+
+
+#include "val.specs.hpp"
+
+
+int main() {
+  return lest::run(specs);
+}
+
+
